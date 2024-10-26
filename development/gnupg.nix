1f6d4459586d57663048ab6834986d9815d059c4
Jan B <git@keksgesicht.net>
2024-10-26 21:45:16 +0200

    save hints on how to create, edit and update PGP keys with GnuPG

--- development/gnupg.nix
+++ development/gnupg.nix
@@ -1,9 +1,24 @@
-{ config, ... }:
+{ ... }:
 
+# https://mikeross.xyz/create-gpg-key-pair-with-subkeys/
+# https://github.com/drduh/YubiKey-Guide?tab=readme-ov-file#updating-keys
 {
   # make signed commits
   services.pcscd.enable = true;
-  programs.gnupg.agent = {
-    enable = true;
-  };
+  programs.gnupg.agent.enable = true;
+
+  /*
+  gpg --full-generate-key
+
+  gpg --homedir $(mktemp -d) --import ~/.secrets/gpg/<mail>.secret.gpg
+  gpg --list-keys
+  gpg --edit-key <mail>
+
+  gpg --fingerprint <mail> | head -n 2 | tail -n 1
+  gpg --armor --export <mail> > ~/.secrets/gpg/<mail>.pubkey.gpg
+  gpg --keyserver keys.openpgp.org --send-key <mail>
+
+  gpg --export-secret-keys    <mail> > ~/.secrets/gpg/<mail>.secret.gpg
+  gpg --export-secret-subkeys <mail> > ~/.secrets/gpg/<mail>.secsub.gpg
+  */
 }
