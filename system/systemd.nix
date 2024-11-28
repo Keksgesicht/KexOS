d52be0cab7ccb73596ef2b52cbec67bdfa2cf8a4
Jan B. <git@keksgesicht.de>
2024-11-28 23:31:52 +0100

    raise journal log limit

--- system/systemd.nix
+++ system/systemd.nix
@@ -1,27 +1,28 @@
-# file: system/systemd.nix
-# desc: changes to systemd, journald, resolved
 { config, ... }:
 
+let
+  cfgNetName = config.networking.hostName;
+in
 {
   # desktop / server
   services.journald.extraConfig =
-    if (config.networking.hostName == "cookieclicker") then
+    if (cfgNetName == "cookieclicker") then
       ''
-      SystemMaxUse=4G
+      SystemMaxUse=10G
       SystemKeepFree=16G
-      SystemMaxFiles=512
+      SystemMaxFiles=768
       ''
-    else if (config.networking.hostName == "cookiethinker") then
+    else if (cfgNetName == "cookiethinker") then
       ''
       SystemMaxUse=2G
       SystemKeepFree=8G
-      SystemMaxFiles=512
+      SystemMaxFiles=300
       ''
     else
       ''
       SystemMaxUse=1G
       SystemKeepFree=4G
-      SystemMaxFiles=64
+      SystemMaxFiles=100
       ''
   ;
 }
