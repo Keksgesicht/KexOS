141b247d9269ec9324027aed91d97e7f032c7e6b
Jan B. <git@keksgesicht.de>
2025-04-26 23:04:46 +0200

    speed up (re)builds but still reduce storage space through hard-linking

--- nix/basic.nix
+++ nix/basic.nix
@@ -7,28 +7,32 @@
   };
 
   nix = {
-    # https://search.nixos.org/options?query=nix.daemon*
     # make system useable during (re)build
     daemonCPUSchedPolicy = "idle";
     daemonIOSchedClass = "idle";
     daemonIOSchedPriority = 7;
 
-    # dublicates by creating hardlinks for matching files
+    # https://nixos.wiki/wiki/Storage_optimization
+    # removes duplicates by creating hardlinks for matching files
     # $AUTH nix-store --optimise
-    settings.auto-optimise-store = true;
+    optimise = {
+      automatic = true;
+      dates = [ "12:34" ];
+    };
+    #settings.auto-optimise-store = true;
 
     # enable flake commands
     settings.experimental-features = [ "nix-command" "flakes" ];
   };
 
-  # running out of space when building larger projects
-  # e.g. chromium for robotnix
-  # run `nix build` with `--keep-failed` and do this:
-  #   $AUTH mkdir -p /var/tmp/nix-daemon
-  #   $AUTH rsync -avHAX --remove-source-files /tmp/nix-daemon/ /var/tmp/nix-daemon/
-  #   $AUTH mount --bind /var/tmp/nix-daemon /tmp/nix-daemon
   systemd = {
-    services.nix-daemon.environment.TMPDIR = "/tmp/nix-daemon";
+    # running out of space when building larger projects
+    # e.g. chromium for robotnix
+    # run `nix build` with `--keep-failed` and do this:
+    #   $AUTH mkdir -p /var/tmp/nix-daemon
+    #   $AUTH rsync -avHAX --remove-source-files /tmp/nix-daemon/ /var/tmp/nix-daemon/
+    #   $AUTH mount --bind /var/tmp/nix-daemon /tmp/nix-daem
+    services."nix-daemon".environment.TMPDIR = "/tmp/nix-daemon";
     tmpfiles.rules = [ "q /tmp/nix-daemon 1777 root root 10d" ];
   };
 }
