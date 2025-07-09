f33dab93c524fd650973a11bcb027f4b19800248
Jan B. <git@keksgesicht.de>
2025-07-09 15:52:03 +0200

    simple way to add custom read-write path to PlayGround
    and add a script to make data persistent

--- packages/nixpak/PlayGround.nix
+++ packages/nixpak/PlayGround.nix
@@ -9,6 +9,25 @@ in
     wrapper = {
       packages = with pkgs; [
         htop git nix rsync
+        (pkgs.writeShellScriptBin "make-persistent" ''
+          while [ -n "$1" ]; do
+            if ! [ -e "$1" ]; then
+              echo "\"$1\" does not exist!"
+              continue
+            fi
+            DIR_TGT="$HOME/.persistence/$1"
+            DIR_PAR=$(dirname "$DIR_TGT")
+            mkdir -p "$DIR_PAR"
+            if [ -e "$DIR_TGT" ]; then
+              echo "\"$DIR_TGT\" already exists!"
+            else
+              mv -v $1 "$DIR_TGT"
+            fi
+            ln -sv "$DIR_TGT" "$1"
+            shift
+          done
+          exit 0
+        '')
       ];
       variables = {
         #GDK_BACKEND = "x11";
@@ -60,11 +79,6 @@ in
         (sloth.concat' sloth.homeDir "/texmf")
         # browser read-only
         (sloth.mkdir (sloth.concat' sloth.homeDir "/Downloads/read-only"))
-        # maybe read-write
-        (sloth.concat' sloth.homeDir "/devel")
-        (sloth.concat' sloth.homeDir "/git")
-        #(sloth.concat' sloth.homeDir "/Documents")
-        #(sloth.concat' sloth.homeDir "/Module")
       ];
       bind.rw = [
         # .zhistory
@@ -72,8 +86,16 @@ in
           (sloth.mkdir (appDir name))
           ("${ssd-mnt}${home-dir}")
         ]
+        # home persistence
+        [
+          (sloth.mkdir (appDir name))
+          (sloth.concat' sloth.homeDir "/.persistence")
+        ]
         # browser read-write
         (sloth.mkdir (sloth.concat' sloth.homeDir "/Downloads/read-write"))
+        # selectable read-write path
+        (sloth.env "PWD")
+        (sloth.envOr "NIXPAK_PLAYGROUND_PATH_RW" "/a/b/c")
       ];
     };
   };
