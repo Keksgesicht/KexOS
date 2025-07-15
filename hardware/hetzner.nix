29be7cc5b984cf1d7782c5557a88d06b8686a63a
Jan B. <git@keksgesicht.de>
2025-07-15 17:21:30 +0200

    either auto reboot or reduce hung tasks on vhost

--- hardware/hetzner.nix
+++ hardware/hetzner.nix
@@ -48,4 +48,19 @@ in
       inherit serviceConfig;
     };
   };
+
+  # https://wiki.archlinux.org/title/sysctl
+  # https://man7.org/linux/man-pages/man5/proc.5.html
+  boot.kernel.sysctl = {
+    # reboot on panic after x seconds
+    "kernel.panic" = 3;
+
+    # https://www.jethrocarr.com/2019/01/05/automatically-restarting-gnu-linux-hosts-upon-hung-storage/
+    "kernel.hung_task_panic" = 1;
+
+    # https://forum.proxmox.com/threads/vm-blocked-due-to-hung_task_timeout_secs.22488/
+    # https://blog.ronnyegner-consulting.de/2011/10/13/info-task-blocked-for-more-than-120-seconds/comment-page-1/
+    "vm.dirty_ratio" = 10;
+    "vm.dirty_background_ratio" = 5;
+  };
 }
