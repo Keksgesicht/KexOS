93556e754dbd06076205026d712b8c6afbc1b0b6
Jan B. <git@keksgesicht.de>
2025-08-05 10:31:00 +0200

    nixpkgs 25.11 changes: watchdog option renamed

--- hardware/security/watchdog.nix
+++ hardware/security/watchdog.nix
@@ -2,9 +2,9 @@
 
 {
   # https://unix.stackexchange.com/questions/694464/how-to-make-systemd-to-stop-kicking-the-hardware-watchdog
-  systemd.watchdog = {
-    #device = "/dev/watchdog";
-    runtimeTime = "2min";
-    rebootTime  = "3min";
+  systemd.settings.Manager = {
+    #WatchdogDevice = "/dev/watchdog";
+    RebootWatchdogSec  = "3min";
+    RuntimeWatchdogSec = "2min";
   };
 }
