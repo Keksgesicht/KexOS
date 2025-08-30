771ba9f02ba1f977d9f52cd911be457ec1562648
Jan B. <git@keksgesicht.de>
2025-08-31 01:43:08 +0200

    refactor DevShell script: allow dir path as part of result suffix

--- development/base-devel.nix
+++ development/base-devel.nix
@@ -33,20 +33,35 @@
         echo "$0 path [--suffix=name] args.."
       }
 
+      myDir="."
       myOpts=""
+      resPath=""
       if [ -d "$1" ]; then
         myDir="$1"
         shift
       fi
       if [ "$1" = "--suffix" ]; then
         shift
-        myOpts+=" --profile "result-$1""
+        if [ -z "$1" ]; then
+          usage
+        fi
+        res_dir=$(dirname "$1")
+        res_file=$(basename "$1")
         shift
+        resPath="$res_dir/result-$res_file"
       fi
-      myOpts+=" $@"
+      myOpts+="$@"
 
-      exec -a nix \
-        nix develop ''${myDir} ''${myOpts} --command zsh
+      if [ -z "$resPath" ]; then
+        exec -a nix \
+          nix develop "$myDir" $myOpts \
+          --command zsh
+      else
+        exec -a nix \
+          nix develop "$myDir" $myOpts \
+          --profile "$resPath" \
+          --command zsh
+      fi
     '')
   ];
 }
