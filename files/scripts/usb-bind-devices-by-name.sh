3142d65c1f6901a35c89700fc094aa4fcc8fd734
Jan B. <git@keksgesicht.de>
2025-09-08 12:16:45 +0200

    usb disable script: add support for multiple with same name

--- files/scripts/usb-bind-devices-by-name.sh
+++ files/scripts/usb-bind-devices-by-name.sh
@@ -29,15 +29,16 @@ fi
 
 usb_sys_path="/sys/bus/usb"
 
-usb_line=$(lsusb | grep "${device_name}" | head -n 1 | tr ' ' '\n' | grep -E '[0-9a-f]{4}:[0-9a-f]{4}')
-usb_id_vendor=$(echo "${usb_line}" | cut -d ':' -f1)
-usb_id_device=$(echo "${usb_line}" | cut -d ':' -f2)
+lsusb | grep "${device_name}" | tr ' ' '\n' | grep -E '[0-9a-f]{4}:[0-9a-f]{4}' | sort | uniq | while IFS="" read -r usb_line; do
+	usb_id_vendor=$(echo "${usb_line}" | cut -d ':' -f1)
+	usb_id_device=$(echo "${usb_line}" | cut -d ':' -f2)
 
-grep -r . "${usb_sys_path}/devices/"*/idVendor | grep "${usb_id_vendor}" | while IFS="" read -r vendorPath; do
-	usbDir=$(dirname "$vendorPath")
-	if ! [ "$(cat "${usbDir}/idProduct")" = "${usb_id_device}" ]; then
-		continue
-	fi
-	usbSysFile=$(basename "$usbDir")
-	echo "${usbSysFile}" | tee "${usb_sys_path}/drivers/usb/${mode}"
+	grep -r . "${usb_sys_path}/devices/"*/idVendor | grep "${usb_id_vendor}" | while IFS="" read -r vendorPath; do
+		usbDir=$(dirname "$vendorPath")
+		if ! [ "$(cat "${usbDir}/idProduct")" = "${usb_id_device}" ]; then
+			continue
+		fi
+		usbSysFile=$(basename "$usbDir")
+		echo "${usbSysFile}" | tee "${usb_sys_path}/drivers/usb/${mode}"
+	done
 done
