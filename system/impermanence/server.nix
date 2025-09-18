583a316bc463ebf70dec39edd7fb4d63cb7be0b7
Jan B. <git@keksgesicht.de>
2025-09-18 12:07:25 +0200

    add nixpkgs directories on other systems too
    they might not night it directly, but flake refuses to build without it

--- system/impermanence/server.nix
+++ system/impermanence/server.nix
@@ -3,15 +3,12 @@
 {
   # https://nixos.wiki/wiki/Impermanence#Home_Managing
   # https://github.com/nix-community/impermanence
-  environment.persistence = {
-    "${ssd-mnt}" = {
-      hideMounts = true;
-      # do not even try using the home-manager impermanence module
-      users."${username}" = {
-        directories = [
-          "nixos-config"
-        ];
-      };
-    };
+  environment.persistence."${ssd-mnt}" = {
+    hideMounts = true;
+    # do not even try using the home-manager impermanence module
+    users."${username}".directories = [
+      "nixos-config"
+      "git/hdd/nix/nixpkgs"
+    ];
   };
 }
