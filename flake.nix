a5f05ffcb083edf53c19acdd4f0a2e03dc7cd4ba
Jan Braun <git@keksgesicht.de>
2025-12-15 23:27:17 +0100

    fix Plasma 6.5 issues with CopyQ
    it was nixpkgs packaging issue not CopyQ or KDE Plasma

--- flake.nix
+++ flake.nix
@@ -159,7 +159,6 @@
         system = "x86_64-linux";
         specialArgs = (myArgs system) // {
           isDesktop = true;
-          holidayMode = true;
           lan-ip-suf = "220";
           vpn-ip-suf = "1";
           ifLan = "enp4s0";
@@ -176,7 +175,6 @@
         system = "x86_64-linux";
         specialArgs = (myArgs system) // {
           isDesktop = true;
-          holidayMode = true;
           vpn-ip-suf = "102";
           lan-ip-suf = "1";
           ifLan = "enp2s0";
