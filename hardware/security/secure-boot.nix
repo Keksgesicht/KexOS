34e13736869e2a00aae265209cde11e1f449fdfa
Jan Braun <git@keksgesicht.de>
2026-06-23 11:33:34 +0200

    update lanzaboote

--- hardware/security/secure-boot.nix
+++ hardware/security/secure-boot.nix
@@ -8,7 +8,6 @@
   # https://nixos.wiki/wiki/Secure_Boot
   # https://github.com/nix-community/lanzaboote
   boot = if (builtins.hasAttr "lanzaboote" options.boot) then {
-    bootspec.enable = true;
     loader.systemd-boot.enable = lib.mkForce false;
     lanzaboote = {
       enable = true;
