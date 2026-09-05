5b314c5f978f7f9f32a340bce3f7a5df9491eac6
Jan Braun <git@keksgesicht.de>
2026-09-05 14:37:41 +0200

    nixpkgs 26.11 changes: journald settings

--- system/systemd.nix
+++ system/systemd.nix
@@ -5,30 +5,22 @@ let
 in
 {
   # desktop / server
-  services.journald.extraConfig =
-    if (cfgNetName == "cookieclicker") then
-      ''
-      SystemMaxUse=10G
-      SystemKeepFree=16G
-      SystemMaxFiles=768
-      ''
-    else if (cfgNetName == "cookiethinker") then
-      ''
-      SystemMaxUse=2G
-      SystemKeepFree=8G
-      SystemMaxFiles=300
-      ''
-    else if (cfgNetName == "cookiemailer") then
-      ''
-      SystemMaxUse=512M
-      SystemKeepFree=1G
-      SystemMaxFiles=50
-      ''
-    else
-      ''
-      SystemMaxUse=1G
-      SystemKeepFree=4G
-      SystemMaxFiles=100
-      ''
-  ;
+  services.journald.settings.Journal =
+    if (cfgNetName == "cookieclicker") then {
+      SystemMaxUse   = "10G";
+      SystemKeepFree = "16G";
+      SystemMaxFiles = 768;
+    } else if (cfgNetName == "cookiethinker") then {
+      SystemMaxUse   = "2G";
+      SystemKeepFree = "8G";
+      SystemMaxFiles = 300;
+    } else if (cfgNetName == "cookiemailer") then {
+      SystemMaxUse   = "512M";
+      SystemKeepFree = "1G";
+      SystemMaxFiles = 50;
+    } else {
+      SystemMaxUse   = "1G";
+      SystemKeepFree = "4G";
+      SystemMaxFiles = 100;
+  };
 }
